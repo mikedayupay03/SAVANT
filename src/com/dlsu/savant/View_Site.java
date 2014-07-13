c7acdcdad16673a6a2e2be7d1ee9eb6a91f8fa9c
daps03 <mike.dayupay03@gmail.com>
2014-07-13 17:03:47 +0800

    Creating a new site
    Tried creating a new site. Failed.

--- src/com/dlsu/savant/View_Site.java
+++ src/com/dlsu/savant/View_Site.java
@@ -1,21 +1,34 @@
 package com.dlsu.savant;
 
 
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class View_Site extends Activity {
 	
-	TextView nameOfSite;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view__site);
 		
-		nameOfSite = (TextView)findViewById(R.id.nameOfSite);
-		nameOfSite.setText("Leron");
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		Intent intent = getIntent();
+		String siteName = intent.getStringExtra(Create_Site.EXTRA_MESSAGE);
+		//String munName = intent.getStringExtra(Create_Site.EXTRA_MESSAGE);
+		//String provName = intent.getStringExtra(Create_Site.EXTRA_MESSAGE);
+		TextView site = (TextView)findViewById(R.id.nameOfSite);
+		//TextView mun = (TextView)findViewById(R.id.nameOfMun);
+		//TextView date = (TextView)findViewById(R.id.nameOfDate);
+		
+		site.setText(siteName);
+		//mun.setText(munName +", " +provName);
+		//date.setText(Calendar.DATE);
+		
 	}
 }
